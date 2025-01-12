b40190aefe446fd74e408e4160aba88ed6b30124
kjdiaz508 <diazkevin508@gmail.com>
2025-01-12 14:54:02 -0800

    lab 1 done

--- GradiationRSVP/app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
+++ GradiationRSVP/app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
@@ -3,13 +3,20 @@ package com.zybooks.graduationrsvp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.zybooks.graduationrsvp.ui.theme.GraduationRSVPTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,8 +38,18 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GradScreen(modifier: Modifier = Modifier) {
     Text(
-        text = "Graduation Announcement",
-        modifier = modifier
+        text = stringResource(R.string.graduation_announcement),
+        fontSize = 50.sp,
+        modifier = modifier,
+        lineHeight = 50.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color.Red,
+        textAlign = TextAlign.Center
+    )
+    Image(
+        painter = painterResource(R.drawable.grad_cap),
+        contentDescription = "Graduation cap",
+        alpha = 0.3f
     )
 }
 
